67faac2538ae96571500d20182b1471b1a792a73
Vitor Suzarte <vitorvsuzarte@gmail.com>
2026-02-09 08:58:42 -0300

    Palindrome solution

---
+++ LeetCode/Algorithms/Palindrome/Palindrome.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Algorithms.Palindrome;
+
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        var str = x.ToString();
+        var strLength = str.Length - 1;
+
+        for (var i = 0; i <= strLength; i++)
+        {
+            var oppositeChar = strLength - i;
+
+            if (str[i] != str[oppositeChar])
+                return false;
+
+            if (i > strLength / 2)
+                break;
+        }
+
+        return true;
+    }
+}
+
+public static class Palindrome
+{
+    public static void Solution(int x)
+    {
+        var solution = new Solution();
+        Console.WriteLine(solution.IsPalindrome(x));
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,3 +1,5 @@
-﻿using LeetCode.Algorithms.TwoSum;
-TwoSum.Solution([3,2,4], 6);
+﻿// using LeetCode.Algorithms.TwoSum;
+// TwoSum.Solution([3,2,4], 6);
 
+using LeetCode.Algorithms.Palindrome;
+Palindrome.Solution(10);
