f94f1b990d447c5f9469a70919d154d7730aa090
Vitor Suzarte <vitorvsuzarte@gmail.com>
2026-02-18 17:58:24 -0300

    BubbleSort

---
+++ SortingAlgos/BubbleSort/Bubble.cs
@@ -0,0 +1,30 @@
+﻿namespace SortingAlgos.BubbleSort;
+
+public static class Bubble
+{
+    public static int[] Sort(int[] numbers)
+    {
+        var i = 0;
+        var j = numbers.Length;
+        
+        while (j > 0)
+        {
+            if (i + 1 >= j)
+            {
+                i = 0;
+                j--;
+            }
+
+            if (numbers[i] <= numbers[i + 1])
+            {
+                i++;
+                continue;
+            }
+
+            (numbers[i], numbers[i + 1]) = (numbers[i + 1], numbers[i]);
+            i++;
+        }
+
+        return numbers;
+    }
+}
---
+++ SortingAlgos/Program.cs
@@ -0,0 +1,6 @@
+﻿using SortingAlgos.BubbleSort;
+
+var result = Bubble.Sort([5, 3, 4, 2, 1]);
+
+foreach (var num in result)
+    Console.WriteLine(num);
